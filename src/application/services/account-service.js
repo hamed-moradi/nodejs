19b667c8e70cc6ac014cd95e8dc31dc2505d88ce
hamèd <hamed-moradi@live.com>
2021-01-21 01:28:25 +0330

    for sync 1

--- src/application/services/account-service.js
+++ src/application/services/account-service.js
@@ -2,12 +2,18 @@
 import context from '../../domain/_app/mssql-context.js';
 import account from "../../domain/entities/account.js";
 
+let signin = async (model) => {
+    var query = `select * from dbo.[account] where username = '${model.username}'`;
+    var result = await context.query(query);
+    return result != null && result != undefined ? result.recordset[0] : null;
+}
+
 let getById = async (id) => {
     //var props[];
     //props[0] = new sqlmodel('in', 'Id', context.mssql.Int, id);
     //var query = `select top(1) * from dbo.[user] where id = ${id}`;
     //var result = await context.query(query, props);
-    var result = await context.query(`select top(1) * from dbo.[user] where id = ${id}`);
+    var result = await context.query(`select top(1) * from dbo.[account] where id = ${id}`);
     //var result = await context.query(`select * from user where id = ${id} limit 1`);
     return result != null && result != undefined ? result.recordset[0] : null;
 };
@@ -15,7 +21,7 @@ let getById = async (id) => {
 let getAll = async () => {
     await context.pool.connect();
     var request = new context.mssql.Request(context.pool);
-    var result = await request.query(`select * from dbo.[user]`);
+    var result = await request.query(`select * from dbo.[account]`);
     context.pool.close();
     return result.recordset;
 };
@@ -35,4 +41,4 @@ let insert = async (model) => {
     return result;
 };
 
-export default { getById, getAll, update, insert};
+export default { getById, getAll, update, insert };
